ec591d678666b581a6a56f2b9402458e809b8367
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-03-22 21:35:42 +0900

    2021-03-22 에디터 이미지 삭제시 이벤트

--- mango3/src/main/resources/static/js/board/write.js
+++ mango3/src/main/resources/static/js/board/write.js
@@ -1,5 +1,5 @@
-const editorElem = document.querySelector('#editor') 
-const writeFrmElem = document.querySelector('#writeFrm')
+const editorElem = document.querySelector('#editor');
+const writeFrmElem = document.querySelector('#writeFrm');
 
 class MyUploadAdapter {
 	constructor( loader ) {        
@@ -32,6 +32,8 @@ class MyUploadAdapter {
  	abort() {}
 }
 
+
+
 function MyCustomUploadAdapterPlugin(editor) {
     editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
         // Configure the URL to the upload script in your back-end here!
@@ -39,13 +41,68 @@ function MyCustomUploadAdapterPlugin(editor) {
     }
 }
 
+function onImageRemoveEvent(event) {
+	const differ = event.source.differ
+
+    // if no difference
+    if (differ.isEmpty) {
+        return;
+    }
+
+    const changes = differ.getChanges({
+        includeChangesInGraveyard: true
+    });
+
+    if (changes.length === 0) {
+        return;
+    }
+
+    let hasNoImageRemoved = true
+
+    // check any image remove or not
+    for (let i = 0; i < changes.length; i++){
+        const change = changes[i]
+        // if image remove exists
+        if (change && change.type === 'remove' && change.name === 'image') {
+            hasNoImageRemoved = false
+            break
+        }
+    }
+
+    // if not image remove stop execution
+    if (hasNoImageRemoved) {
+        return;
+    }
+
+    // get removed nodes
+    const removedNodes = changes.filter(change => (change.type === 'insert' && change.name === 'image'))
+
+    // removed images src
+    const removedImagesSrc = [];
+    // removed image nodes
+    const removedImageNodes = []
+
+    removedNodes.forEach(node => {
+        const removedNode = node.position.nodeAfter
+        removedImageNodes.push(removedNode)
+        removedImagesSrc.push(removedNode.getAttribute('src'))
+    })
+
+	//서버 이미지 삭제처리
+	removedImagesSrc.forEach(src => {
+		console.log(`src : ${src}`);
+	})
+} 
+
 ClassicEditor
 	.create(editorElem, {
 		  extraPlugins: [MyCustomUploadAdapterPlugin],
 	})
 	.then( editor => {
 	        console.log( editor );
+			editor.model.document.on('change:data', onImageRemoveEvent);
 	})
 	.catch( error => {
 	        console.error( error );
 	})
+	
